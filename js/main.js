ece06576ce19268c41ce3a42e78e8a8232cdb204
Ksenia <110181700+Mishalowo@users.noreply.github.com>
2026-08-03 15:30:13 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -334,7 +334,7 @@ $(document).ready(function() {
 
     // ===== 7. КОРЗИНА =====
     function updateCartBadge() {
-        const totalItems = cart.reduce((sum, item) => sum + (item.quantity || 1), 0);
+        //const totalItems = cart.reduce((sum, item) => sum + (item.quantity || 1), 0);
         const totalSum = cart.reduce((sum, item) => sum + (item.price * (item.quantity || 1)), 0);
         const badge = $('#cart-badge');
         
@@ -385,7 +385,7 @@ $(document).ready(function() {
                 name: name,
                 price: price,
                 image: image,
-                quantity: 0
+                quantity: 1
             });
         }
         
